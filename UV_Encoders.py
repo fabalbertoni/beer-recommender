3a1eca0484a8615752d2d7d0cfd50043079d5559
fabalbertoni <falbertoni811@gmail.com>
2021-03-30 21:46:59 -0300

    Add GraphRec model files

---
+++ UV_Encoders.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+import torch.nn.functional as F
+
+
+class UV_Encoder(nn.Module):
+
+    def __init__(self, features, embed_dim, history_uv_lists, history_r_lists, aggregator, cuda="cpu", uv=True):
+        super(UV_Encoder, self).__init__()
+
+        self.features = features
+        self.uv = uv
+        self.history_uv_lists = history_uv_lists
+        self.history_r_lists = history_r_lists
+        self.aggregator = aggregator
+        self.embed_dim = embed_dim
+        self.device = cuda
+        self.linear1 = nn.Linear(2 * self.embed_dim, self.embed_dim)  #
+
+    def forward(self, nodes):
+        tmp_history_uv = []
+        tmp_history_r = []
+        for node in nodes:
+            tmp_history_uv.append(self.history_uv_lists[int(node)])
+            tmp_history_r.append(self.history_r_lists[int(node)])
+
+        # TODO transform the list of ratings to embeddings, or directly pass to them to the network below. Probably the 2nd works just Ok.
+        # ^ Yes, the aggregator computes the embedding from the list of ratings
+
+        neigh_feats = self.aggregator.forward(nodes, tmp_history_uv, tmp_history_r)  # user-item network
+
+        self_feats = self.features.weight[nodes]
+        # self-connection could be considered.
+        combined = torch.cat([self_feats, neigh_feats], dim=1)
+        combined = F.relu(self.linear1(combined))
+
+        return combined
